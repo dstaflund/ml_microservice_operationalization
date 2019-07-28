96a5de5f3a595de6f3d1f20371f9e85787f31582
Darryl Staflund <darryl.staflund@hushmail.com>
2019-07-28 12:13:32 -0600

    fix: Remove command at end of my run script as it was overwriting Dockerfile CMD statement

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
 docker build --tag dstaflund/prediction:1.0 .
 docker images --all
-docker run -it -p 8000:80 dstaflund/prediction:1.0 bash
+docker run -it -p 8000:80 dstaflund/prediction:1.0
